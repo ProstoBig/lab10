f64c255147eb2c4f02b846a70073be49088d683d
ProstoBig <134418315+ProstoBig@users.noreply.github.com>
2023-12-20 16:41:04 +0200

    1

--- src/lab10/Contact.java
+++ src/lab10/Contact.java
@@ -1,8 +1,12 @@
 package lab10;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * Клас, що представляє контактну інформацію про особу.
@@ -84,10 +88,44 @@ public class Contact {
      * Відомості про контакт додаються до файлу у форматі: ім'я, дата народження, номер телефону.
      */
     public void saveContactToFile() {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\Users\\Олександр\\Desktop\\contacts.txt", true))) {
+        String userHome = System.getProperty("user.home");
+        String desktopPath = userHome + File.separator + "Desktop";
+        String filePath = desktopPath + File.separator + "contacts.txt";
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true))) {
             writer.write(name + "," + birthDate + "," + phoneNumber + "\n");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+    static boolean isPhoneNumberExists(String filePath, String phoneNumber) {
+        try {
+            byte[] fileBytes = Files.readAllBytes(Paths.get(filePath));
+            String fileContent = new String(fileBytes, StandardCharsets.UTF_8);
+
+            String[] contacts = fileContent.split("\n");
+            for (String contact : contacts) {
+                String[] contactDetails = contact.split(",");
+                if (contactDetails.length >= 3 && contactDetails[2].equals(phoneNumber)) {
+                    return true;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+    public static boolean isNameExists(String filePath, String name) {
+        try {
+            byte[] fileBytes = Files.readAllBytes(Paths.get(filePath));
+            String fileContent = new String(fileBytes, StandardCharsets.UTF_8);
+
+            // Перевірка наявності імені у файлі
+            return fileContent.contains(name);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 }
+
